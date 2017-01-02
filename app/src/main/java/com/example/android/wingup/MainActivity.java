8f6c934c9e6f6a6c129bdb322eace438721ab456
Victoria <it14121@uom.edu.gr>
2017-01-02 12:16:52 +0200

    Two Static autocompleteTextviews..

--- app/src/main/java/com/example/android/wingup/MainActivity.java
+++ app/src/main/java/com/example/android/wingup/MainActivity.java
@@ -2,12 +2,35 @@ package com.example.android.wingup;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    AutoCompleteTextView autocompleteFrom;
+    AutoCompleteTextView autocompleteTo;
+
+    String[] arr = { "RHO - Rhodes", "SKG - Thessaloniki", "ATH - Athens"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        autocompleteFrom = (AutoCompleteTextView)
+                findViewById(R.id.from_edit);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>
+                (this,android.R.layout.select_dialog_item, arr);
+
+        autocompleteFrom.setThreshold(2);
+        autocompleteFrom.setAdapter(adapter);
+
+        autocompleteTo = (AutoCompleteTextView)
+                findViewById(R.id.to_edit);
+
+        autocompleteTo.setThreshold(2);
+        autocompleteTo.setAdapter(adapter);
     }
 }
